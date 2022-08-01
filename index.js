3ed4139aca1df47ce1b114e376c7f6774c79c9cf
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-08-01 01:00:14 -0500

    creando las rutas. 5

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const port = 3000;
 
 app.get('/', (request, response) => {
     response.send('Hola mi server en express')
+})
 
+app.get('/nueva-ruta', (request, response) => {
+    response.send('Hola soy una nueva ruta')
+})
+
+app.get('/products', (request, response) => {
+    response.json({
+        name: 'Product 1',
+        price: 1580
+    })
 })
 
 app.listen(port, () => {
