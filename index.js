78006a0a94defb6fb13dd58d6af63044d540cb30
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-08-01 03:04:40 -0500

    metodo post. 10

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const routerAppi  = require('./routes'); //ya sabe que debe ir a index.js
 const app = express()
 const port = 3000;
 
+app.use(express.json())
+
 app.get('/', (request, response) => {
     response.send('Hola mi server en express')
 })
