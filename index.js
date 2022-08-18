2ed33047f7142e59a45e49cad1e616b39ec53849
Alejandro Castaneda <alejandroxd62@gmail.com>
2022-08-17 20:07:18 -0500

    cors. 6 y 5

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const routerAppi  = require('./routes'); //ya sabe que debe ir a index.js
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
@@ -7,7 +8,17 @@ const app = express()
 const port = 3000;
 
 app.use(express.json()) //para que el post pueda enviarnos su body
-
+const whiteList = ['http://localhost:8080', 'https://myapp.co']
+const options = {
+    origin: (origin, callback) => {
+        if (whiteList.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error('No permitido'))
+        }
+    } 
+}
+app.use(cors(options))
 app.get('/', (request, response) => {
     response.send('Hola mi server en express')
 })
